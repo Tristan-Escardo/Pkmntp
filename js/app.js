4a4959ba25642980a860934305fa087afd6e89ec
Tristan-Escardo <triescardo@gmail.com>
2022-09-28 13:29:21 +0200

    boucle while

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
-const pokemon = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0%27'
-const pokemons = []
+let pokemon = []
+fetch('https://pokeapi.co/api/v2/pokemon')
+.then(response => response.json())
+.then(data => {
+    for (var i in data.results) {
+        pokemon.push(JSON.stringify(data.results[i]))
+        document.getElementById('test_name').textContent = pokemon
+    }
+})
 
-function getAllPokemon(){
-    fetch(pokemon)
-        .then(response => response.json())
-        .then(data => pokemons.push(...data.pokemon))
+var k = 0
+var j = 9
+let Name = []
+
+while (k < pokemon.lenght) {
+    if (pokemon[k] [j] != '"'){
+        Name.push(pokemon[k] [j])
+        j++;
+    } else {
+        j++;
+    }
+
+    if (pokemon[k] [j] == '}') {
+        k++;
+        j = 9
+    }
 }
+
+console.log(Name)
